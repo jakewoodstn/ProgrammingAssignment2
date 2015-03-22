fa3d2731b391823ebcc1a5d64119a908a1a25b4b
jakewoodstn <jake.woods@comcast.net>
2015-03-22 15:36:26 -0500

    adding body to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to satisfy conditions of R programming course rprog-012
+## assignment #2
 
-## Write a short comment describing this function
-
+## creates an "object" to store a matrix and its inverse for later use 
 makeCacheMatrix <- function(x = matrix()) {
-
+     inv<-NULL
+     set<-function(y){
+          x<<-y
+          inv<<-NULL
+     }
+     get<-function() x
+     setInv<-function(inverted) inv<<-inverted
+     getInv<-function() inv
+     list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve solves for the inverse if necessary and caches the value, 
+##otherwise reads and returns cached value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     
+     ##safety - if non list is passed in (makeCacheMatrix not called)
+     ##then call it.
+     repx<-x
+     if (!is.list(x)){
+          repx<-makeCacheMatrix(x)
+     }
+     x<-repx
+     
+     inv <- x$getInv()
+     if(!is.null(inv)){
+          message ("getting cached inverse")
+     } else {
+          message("solving and caching inverse")
+          mat <- x$get()
+          inv <- solve(mat)
+          x$setInv(inv)
+     }
+     inv
 }
